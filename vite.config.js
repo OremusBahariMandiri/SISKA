010822f2599b184073a540714943c311f60c5356
Jamas108 <oremus.itdept@gmail.com>
2025-10-20 09:35:50 +0700

    port

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,11 @@ export default defineConfig({
         host: '0.0.0.0',
         hmr: {
             // host:'192.168.0.39' // host JAMAS
-            host: '192.168.1.17' // host OREMUS
+            host: '192.168.1.24' // host OREMUS
         },
         cors: {
             // origin: ['http://192.168.0.39:8080', 'http://127.0.0.1:8000'], // Server JAMAS
-            origin: ['http://192.168.1.17:8082', 'http://127.0.0.1:8000'], // SERVER OREMUS
+            origin: ['http://192.168.1.24:8082', 'http://127.0.0.1:8000'], // SERVER OREMUS
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
             credentials: true
         }
